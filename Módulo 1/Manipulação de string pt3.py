0bd058dbb31e84d2677b6e9b5b874a499136887e
Gabriel Franco <fgabrielc.05@gmail.com>
2021-12-03 22:30:05 -0300

    Create Manipulação de string pt3.py

---
+++ Módulo 1/Manipulação de string pt3.py
@@ -0,0 +1,5 @@
+print('=' * 20)
+nome = str(input('\n Qual o seu nome?: ')).strip()
+corte = nome.split()
+print(f'\n Olá {corte[0]} seu ultimo nome {corte[len(corte)-1]} é muito bonito ;)\n')
+print('=' * 20)
