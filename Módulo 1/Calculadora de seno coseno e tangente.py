f65569965993ea16be19e4522d64404e66b07ebe
Gabriel Franco <fgabrielc.05@gmail.com>
2021-12-03 22:23:01 -0300

    Create Calculadora de seno coseno e tangente.py

---
+++ Módulo 1/Calculadora de seno coseno e tangente.py
@@ -0,0 +1,9 @@
+import math
+print('=' * 20)
+print('')
+angulo = float(input(' Digite o angulo: '))
+valortodo = math.radians(angulo)
+print('')
+print(f' Seno: {math.sin(valortodo):.2f}\n Coseno: {math.cos(valortodo):.2f}\n Tangente: {math.tan(valortodo):.2f}')
+print('')
+print('=' * 20)
