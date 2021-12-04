972a0ef999cfec3189921fc14f153078e88cd65e
Gabriel Franco <fgabrielc.05@gmail.com>
2021-12-03 22:17:16 -0300

    Create Calculadora de raiz.py

---
+++ Módulo 1/Calculadora de raiz.py
@@ -0,0 +1,6 @@
+from math import sqrt, floor
+print('=' * 20)
+num = int(input('\n Digite um numero: '))
+raiz = sqrt(num)
+print(f' Número digitado: {num:=^20}\n Raiz qudrada: {floor(raiz):=^20}')
+print('=' * 20)
